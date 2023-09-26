fbb59dc2054ac65f7a48f50e9ef0e1c4135f20cd
HalahanNikolai <133275902+HalahanNikolai@users.noreply.github.com>
2023-09-27 00:18:28 +0300

    ex 1-3

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+import getRandomHexColor from './comon';
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+btnStop.disabled = true;
+let timerId = null;
 
+
+btnStart.addEventListener('click', () => {
+    btnStart.disabled = true;
+    btnStop.disabled = false;
+
+    timerId = setInterval(() => {
+        document.body.style.background = getRandomHexColor();
+    }, 1000);
+});
+
+btnStop.addEventListener('click', () => {
+    clearInterval(timerId);
+    btnStart.disabled = false;
+    btnStop.disabled = true;
+});
